845442124a79c20ab2477ed85098921dc332e042
user <user@users-MacBook-Pro.local>
2022-02-04 14:53:41 +0000

    added in server

---
+++ Server/db/seed.js
@@ -0,0 +1,23 @@
+use lloyds;
+db.dropDatabase();
+
+db.names.insertMany([
+  {
+    question: "question",
+    answer: "answer"
+    
+  },
+
+  {
+    question: "question",
+    answer: "answer"
+    
+  },
+
+  {
+    question: "question",
+    answer: "answer"
+    
+  },
+
+]);
